7d8d8cade05a79f8032210bae1753c6793f2aaf1
Irene Monzon <irene.monzon@lvaindices.com>
2023-12-08 12:28:46 -0300

    Created form and authenticate user, install dependencies

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
+import { ApolloProvider } from '@apollo/client'
+import client from '@/config/apollo'
 import '@/styles/globals.css'
 
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ApolloProvider client={client}>
+       <Component {...pageProps} />
+    </ApolloProvider>
+  )
 }
